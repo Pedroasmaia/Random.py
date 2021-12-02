e4d39ee94ef640bb506c9835df6a74124a0851df
Pedro Augusto <78991545+Pedroasmaia@users.noreply.github.com>
2021-12-01 23:45:57 -0300

    Finalizado Iteração pra Goleiros

--- Jogadores/Time.py
+++ Jogadores/Time.py
@@ -6,12 +6,24 @@ atacantes = []
 
 print('Quantos goleiros seu time vai ter?')
 qtd_goleiros =  int(input())
-print('Quantos zagueiros seu time vai ter?')
-qtd_zagueiros =  int(input())
-print('Quantos meias seu time vai ter?')
-qtd_meias =  int(input())
-print('Quantos atacantes seu time vai ter?')
-qtd_atacantes =  int(input())
 
-if qtd_goleiros > 3:
-    print("Escolha no máximo 3 goleiros")
+if qtd_goleiros <= 3:
+    for qtd_goleiros in range(qtd_goleiros):
+        print('Escolha seu {}° Goleiro:'.format(qtd_goleiros + 1))    
+        goleiros.append(input())
+    print(goleiros)
+elif qtd_goleiros == 0:
+    print('Seu time deve ter no minimo 1 Goleiro')
+else:
+    print('Seu time pode ter no máximo 3 Goleiros')
+    print(goleiros)
+
+
+
+
+#print('Quantos zagueiros seu time vai ter?')
+#qtd_zagueiros =  int(input())
+#print('Quantos meias seu time vai ter?')
+#qtd_meias =  int(input())
+#print('Quantos atacantes seu time vai ter?')
+#qtd_atacantes =  int(input())
