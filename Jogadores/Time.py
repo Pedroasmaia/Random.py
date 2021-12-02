8c692dc0eab8c04f51b4719feaf194563c5ec5b3
Pedro Augusto <pedroasmaia@outlook.com>
2021-12-02 00:34:48 -0300

    Erros no Zagueiros

--- Jogadores/Time.py
+++ Jogadores/Time.py
@@ -5,24 +5,31 @@ meias = []
 atacantes = []
 
 print('Quantos goleiros seu time vai ter?')
-qtd_goleiros =  int(input())
+gol_qtd =  int(input())
 
-if qtd_goleiros <= 3:
-    for qtd_goleiros in range(qtd_goleiros):
-        print('Escolha seu {}° Goleiro:'.format(qtd_goleiros + 1))    
+if gol_qtd <= 3:
+    for gol_qtd in range(gol_qtd):
+        print('Escolha seu {}° Goleiro:'.format(gol_qtd + 1))    
         goleiros.append(input())
     print(goleiros)
-elif qtd_goleiros == 0:
+elif gol_qtd == 0:
     print('Seu time deve ter no minimo 1 Goleiro')
 else:
     print('Seu time pode ter no máximo 3 Goleiros')
-    print(goleiros)
 
+print('Quantos zagueiros seu time vai ter?')
+zag_qtd =  int(input())
 
+if zag_qtd >= 2:
+    for zag_qtd in range(zag_qtd):
+        print('Escolha seu {}° Zagueiro'.format(zag_qtd + 1))
+        zagueiros.append(input())
+    print(zagueiros)
+elif zag_qtd <= 1:
+    print('Seu time deve ter no minimo 2 Zagueiros')
+elif zag_qtd >= 6:
+    print('Seu time pode ter no máximo 5 Zagueiros')
 
-
-#print('Quantos zagueiros seu time vai ter?')
-#qtd_zagueiros =  int(input())
 #print('Quantos meias seu time vai ter?')
 #qtd_meias =  int(input())
 #print('Quantos atacantes seu time vai ter?')
