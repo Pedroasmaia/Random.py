cde1ebaf489ed46ff2dc12fa8ed27ff3f035c7b4
Pedro Augusto <78991545+Pedroasmaia@users.noreply.github.com>
2021-12-03 20:03:06 -0300

    Finalizado | Pode sofrer alterações mas esta OK

--- Pedra_Papel_Tesoura/ppt.py
+++ Pedra_Papel_Tesoura/ppt.py
@@ -14,7 +14,6 @@ for rodadas in range(rodadas):
         jogada1 = None
         print('Escolha um número entre 1 e 3')
         break
-    print('Você jogou {}'.format(jogada1))
     jogada2 = (random.randrange(1,4))
     if jogada2 == 1:
         jogada2 = "Pedra"
@@ -22,5 +21,15 @@ for rodadas in range(rodadas):
         jogada2 = "Papel"
     elif jogada2 == 3:
         jogada2 = "Tesoura"
+    print('Você jogou {}'.format(jogada1))
     print('O Adversario jogou {}'.format(jogada2))
+#Lógica para definir vencedor
+if jogada1 == "Pedra" and jogada2 != "Papel":
+    print("Parabens!! Você ganhou!")
+elif jogada1 == "Papel" and jogada2 != "Tesoura":
+    print("Parabens!! Você ganhou!")
+elif jogada1 == "Tesoura" and jogada2 != "Pedra":
+    print("Parabens!! Você ganhou!")
+else:
+    print("Sinto muito, você perdeu!")
 
