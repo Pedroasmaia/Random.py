91533a05745ee0521bf2519ca586631651017f50
Pedro Augusto <78991545+Pedroasmaia@users.noreply.github.com>
2022-01-17 14:55:15 -0300

    Mensagem por comando "!brava" adiconado

--- 09 - Bot Discord/import discord.py
+++ 09 - Bot Discord/import discord.py
@@ -1,23 +1,33 @@
+from importlib.resources import contents
 from itertools import count
 import discord
+from discord.ext import commands
 import random
 from dotenv import load_dotenv
 import os
 
+#Variaveis ocultas
 load_dotenv()
-
 token = os.getenv('token')
+insta = os.getenv('insta')
 
+
+
+
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+#Mensagens por comandos:
 class MyClient(discord.Client):
     async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
 
         if message.content == '!teste':
-            await message.channel.send('teste')
-   
-
+            await message.channel.send()
+        #brava
+        if message.content == '!brava':
+            ig = random.choice(insta)
+            brava= 'https://www.instagram.com/' + ig
+            await message.channel.send(brava)
+       
 client = MyClient()
 client.run(token)
 
---
+++ 09 - Bot Discord/testes.py
@@ -0,0 +1,9 @@
+import random
+
+insta= ['a']
+
+ig = random.choice(insta)
+
+brava= 'https://www.instagram.com/' + ig
+
+print(brava)
